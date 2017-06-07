4ed56e8a67ee3baeb55f004553febb8e61dc6b20
paritashah14 <paritashah14@yahoo.com>
2017-06-07 17:17:22 -0400

    added api for all

---
+++ keys.js
@@ -0,0 +1,6 @@
+exports.twitterKeys = {
+  consumer_key: 'NpimKl6ZKemNA6Qhk4IGMeUFT',
+  consumer_secret: 'NeMA4u2H18g87RYB0ByyQ178Cp0s4MSlihDKU1ZQZapURt5COc',
+  access_token_key: '872281366406332416-Vqb2Q6F9b5M8eNPXTh9JZ164L1GuSgK',
+  access_token_secret: 'QtuiAHCruZNRXBJlgsxC2tOtjH1ZidpxJKkfn4J3G9grM',
+}
---
+++ liri.js
@@ -0,0 +1,67 @@
+// the new change for code to grab the key
+var fs = require('fs-extra');
+//twitter npm from the documention
+var twitter = require('twitter');
+//spotify npm from the documention  (new spotify)
+var Spotify = require('node-spotify-api');
+// OMDB request for API need a key given in class
+var request = require('request');
+//basically loops through the command line arguments passed in the terminal while executing the file tweets, spotify, movie.
+var command = process.argv[2];
+
+// Getting Twitter Keys from keys.js file 
+var twitterKeys = fs.readFile('keys.js', 'utf8', (err, data) =>{
+	if(err) throw err;
+});
+var twitterKeys = require('./keys');
+
+
+// Twitter from npm documentation
+if (command === 'my-tweets'){
+	// twitter keys stored in variable named client
+	var client = new twitter(
+		twitterKey.twitterKeys
+	);
+	// 
+	var twittertweets = {screen_name: 'rutgersshah'};
+	client.get('statuses/user_timeline', twittertweets, function(error, tweets, response) {
+  if (error) {
+    
+  }
+  
+});
+}
+
+// Spotify from the npm documention from new spotify steps client id/secret
+if (command === 'spotify-this-song'){
+	// song title
+	var songSearch = process.argv[3];
+	var spotify = new Spotify({
+  		id: 'de34deec9fbb44ca8aca101d69a22beb',
+ 		secret: '532dcf12e95f43919967d7579626dc4e'
+	});
+ 
+	spotify.search({ type: 'track', query: songSearch }, function(err, data) {
+  		if (err) {
+    	return console.log('Error occurred: ' + err);
+  	}
+console.log('The Artist: ' + data.tracks.items[0].album.artists[0].name);
+console.log('Song Title: ' + data.tracks.items[0].name);
+console.log('Preview the Song: ' + data.tracks.items[0].preview_url);
+console.log('The Album: ' + data.tracks.items[0].album.name);
+});
+}
+
+// OMDB
+
+if (command === 'movie-this'){
+//OMDB API Key given in class
+var omdbKey = "40e9cece";
+//returns an array containing the command line arguments passed 
+//http://www.omdbapi.com/?apikey='from the omdb site usage section
+request('http://www.omdbapi.com/?apikey=' + omdbKey + '&t=' + inputSearch, function (error, response, body) {
+	console.log('error:', error); 
+	console.log('statusCode:', response); 
+	console.log('body:', body); 
+});
+};
